483c798f0b8d08ee8932a840086003e73d02aa82
motianyi <motianyi99@gmail.com>
2021-05-08 15:31:02 +0800

    add create instance script

---
+++ ansible/create-instances.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2021-grp-31-openrc.sh; ansible-playbook create-instances.yaml
---
+++ ansible/install-dep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+. ./unimelb-comp90024-2021-grp-31-openrc.sh; ansible-playbook install-dep.yaml -i inventory/hosts.ini
