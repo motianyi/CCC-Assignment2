a85c4424d927a640d8c709d71a4a45478dc8e839
motianyi <motianyi99@gmail.com>
2021-05-13 17:52:04 +0800

    -adding creating webserver ansible config

--- ansible/create-instances.sh
+++ ansible/create-instances.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 755 ./inventory
-. ./unimelb-comp90024-2021-grp-31-openrc.sh; ansible-playbook create-instances.yaml
+sudo . ./unimelb-comp90024-2021-grp-31-openrc.sh; ansible-playbook create-instances.yaml
