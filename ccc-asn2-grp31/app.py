a0a7bcc3dd69b831d186db2dfe9bddabc4cc5b9c
motianyi <motianyi99@gmail.com>
2021-05-18 23:12:16 +0800

    update frondend app.py

--- ccc-asn2-grp31/app.py
+++ ccc-asn2-grp31/app.py
@@ -3,15 +3,21 @@ from flask import request
 from flask import render_template
 import utils
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="/")
 
-@app.route('')
+
+@app.route('/')
 def main():
     return render_template('main.html')
 	
-@app.route('/time')
-def get_time():
-	return utils.get_time()
+# @app.route('/time')
+# def get_time():
+# 	return utils.get_time()
 
-# if __name__ == '__main__':
-#     app.run()
+# @app.route('/map.html')
+# def map():
+#     return render_template('map.html')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
