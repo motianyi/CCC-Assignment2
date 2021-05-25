778e21421464615661314d880f8882f6bfb0fb13
1752756228 <52920574+1752756228@users.noreply.github.com>
2021-05-26 00:26:20 +0800

    Delete back_end/couchDB/views directory

--- back_end/couchDB/views/city/map.js
+++
@@ -1,5 +0,0 @@
-function (doc) {
-    if(doc.user_location_first != null) {
-      emit([doc.user_location_first], 1);
-    }
-  }
--- back_end/couchDB/views/useimpact/map.js
+++
@@ -1,5 +0,0 @@
-function (doc) {
-  emit([doc.id, doc._id],
-    {followers: doc.user_followers_count,
-      textlength: doc.text.split(" ").length});
-}
